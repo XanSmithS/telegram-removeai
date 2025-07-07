8fb1fc44ea80538a2dc7c070cdf8951c554c2d71
XanSmithS <125887491+XanSmithS@users.noreply.github.com>
2025-07-08 00:19:51 +0300

    4

--- backend/main.py
+++ backend/main.py
@@ -74,4 +74,4 @@ async def send_to_chat(
 # Запуск при ручном старте
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
