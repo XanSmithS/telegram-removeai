ab1befdca18475f3d3d524e506595dd1e966e21b
XanSmithS <125887491+XanSmithS@users.noreply.github.com>
2025-07-08 00:45:03 +0300

    6

--- backend/main.py
+++ backend/main.py
@@ -73,5 +73,8 @@ async def send_to_chat(
 
 # Запуск при ручном старте
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+
+    port = int(os.environ.get("PORT", 8000))  # берём порт из переменных окружения или 8000 по умолчанию
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
